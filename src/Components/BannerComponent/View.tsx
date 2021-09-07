374a145050b9cdfb1a003a50e8c8db6ad9f29f59
oleksiimalichenko <malichenko.al@gmai.com>
2021-09-07 23:34:23 +0300

    Added banner on main page

--- src/Components/BannerComponent/View.tsx
+++ src/Components/BannerComponent/View.tsx
@@ -1,5 +1,27 @@
-import React, {ReactElement} from "react";
+// Core
+import React, { ReactElement } from 'react';
+// Style
+import './Style.css';
 
 export const View: React.FC = (): ReactElement => {
-    return <div>BannerComponent</div>
-}
+	return (
+		<div className="banner">
+			<div className="banner__wrap">
+				<div className="banner__announcement">
+					<div className="banner__caption">
+						<div className="banner__title">
+							<h2>Plates</h2>
+						</div>
+
+						<p className="banner__subtitle">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+							Aliquam est labore maiores molestias optio ratione reiciendis
+							sed voluptatem. Ad animi aperiam asperiores delectus deserunt
+							inventore numquam voluptatum.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
